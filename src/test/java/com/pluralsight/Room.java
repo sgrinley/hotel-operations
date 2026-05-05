4d22fb72e0717044df199d6959d909b20f39e96b
sgrinley <shamar808@yahoo.com>
2026-05-05 08:47:51 -0700

    feat: Add Variables

--- src/test/java/com/pluralsight/Room.java
+++ src/test/java/com/pluralsight/Room.java
@@ -1,4 +1,21 @@
 package com.pluralsight;
 
 public class Room {
+
+    //      Step 1: Add Variables
+    private int NumberOfBeds;
+    private double Price;
+    private boolean Occupied;
+    private boolean Dirty;
+    private boolean Available;
+
+
+    //      Step 2: Add Constructor
+
+
+    //      Step 3: Add Getters
+
+
+    //      Step 3: Add Setters
+
 }
