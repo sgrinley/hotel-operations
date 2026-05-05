704184d0c7b9c8d40db48528f0cbe8e109cebca3
sgrinley <shamar808@yahoo.com>
2026-05-05 09:17:16 -0700

    feat: Generate Constructor Method

--- src/test/java/com/pluralsight/Reservation.java
+++ src/test/java/com/pluralsight/Reservation.java
@@ -2,17 +2,22 @@ package com.pluralsight;
 
 public class Reservation {
 
-//    Add: Variable (Store Data) -> Noun
+    //    Add: Variable (Store Data) -> Noun
     private String roomType; //    { King/Double }
     private int numberOfNights;
     private boolean isWeekend;
 
+    //    Add: Constructor Method (Function) -> Verb
+    public Reservation(String roomType, int numberOfNights, boolean isWeekend) {
+        this.roomType = roomType;
+        this.numberOfNights = numberOfNights;
+        this.isWeekend = isWeekend;
+    }
 
 
 
 
 
-//    Add: Constructor Method (Function) -> Verb
 //    Add: Getters Method (Function) -> Verb
 //    Add: Setters Method (Function) -> Verb
 }
