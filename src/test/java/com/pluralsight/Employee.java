96aac0f0dd3dcfb67e836a0125059b64ff41b8dc
sgrinley <shamar808@yahoo.com>
2026-05-05 09:52:14 -0700

    feat: Generate Constructor Method

--- src/test/java/com/pluralsight/Employee.java
+++ src/test/java/com/pluralsight/Employee.java
@@ -2,7 +2,7 @@ package com.pluralsight;
 
 public class Employee {
 
-//    Add: Variable (Store Data) -> Noun
+    //    Add: Variable (Store Data) -> Noun
     private int employeeId;
     private String name;
     private String department;
@@ -10,12 +10,20 @@ public class Employee {
     private double hoursWorked;
 
 
+    //    Add: Constructor Method (Function) -> Verb
+    public Employee(int employeeId, String name, String department, double payRat, double hoursWorked) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.department = department;
+        this.payRat = payRat;
+        this.hoursWorked = hoursWorked;
+    }
 
 
 
 
 
-//    Add: Constructor Method (Function) -> Verb
+
 //    Add: Getters Method (Function) -> Verb
 //    Add: Setters Method (Function) -> Verb
 }
