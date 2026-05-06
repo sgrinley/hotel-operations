cc785596792f9a4fa2df358e8d89fdd762dd010a
sgrinley <shamar808@yahoo.com>
2026-05-06 06:44:17 -0700

    feat: Add Variables

---
+++ src/test/java/com/pluralsight/Hotel.java
@@ -0,0 +1,18 @@
+package com.pluralsight;
+
+public class Hotel {
+
+    //    Add: Variable (Store Data) -> Noun
+    private String name;
+    private int numberOfSuites;
+    private int numberOfRooms;
+    private int bookedSuites;
+    private int bookedBasicRooms;
+
+
+
+
+
+
+
+}
