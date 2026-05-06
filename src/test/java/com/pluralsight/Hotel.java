762d5057d41992ce7083750aff1a0ff450662c29
sgrinley <shamar808@yahoo.com>
2026-05-06 06:57:07 -0700

    feat: Generate 2nd Constructor Method

--- src/test/java/com/pluralsight/Hotel.java
+++ src/test/java/com/pluralsight/Hotel.java
@@ -17,7 +17,14 @@ public class Hotel {
         this.bookedSuites = bookedSuites;
         this.bookedBasicRooms = bookedBasicRooms;
     }
-
+    //    Add: 2nd Constructor Method (No Booking Yet - Default [0])
+    public Hotel(String name, int numberOfSuites, int numberOfRooms) {
+        this.name = name;
+        this.numberOfSuites = numberOfSuites;
+        this.numberOfRooms = numberOfRooms;
+        this.bookedSuites = 0;
+        this.bookedBasicRooms = 0;
+    }
 
 
 
