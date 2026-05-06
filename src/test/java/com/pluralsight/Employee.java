f5a75de900057c6902cb3492c1f8a9d57575e686
sgrinley <shamar808@yahoo.com>
2026-05-06 06:40:15 -0700

    feat: Add overloaded methods in employee class.

--- src/test/java/com/pluralsight/Employee.java
+++ src/test/java/com/pluralsight/Employee.java
@@ -1,5 +1,8 @@
 package com.pluralsight;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Employee {
 
     //    Add: Variable (Store Data) -> Noun
@@ -45,20 +48,46 @@ public class Employee {
         return regularPay + overTimePay;
     }
 
-    //    Punch-In
+    //  -----  OVERLOADED PUNCH-IN METHOD  -----
+
+    //    Punch-In (manual)
     public void punchIn(int time) {
         punchInTime = time;
     }
 
-    //    Punch-Out
+    //    Punch-In (automatic)
+    public void punchIn() {
+        LocalDateTime now = LocalDateTime.now();
+        punchInTime = now.getHour() * 60 + now.getMinute(); // convert to minutes
+        System.out.printf("%s Clocked in at %d:%02d%n", name, now.getHour(), now.getMinute());
+    }
+
+    //  -----  OVERLOADED PUNCH-OUT METHOD  -----
+
+    //    Punch-Out (manual)
     public void punchOut(int time) {
         if (time < punchInTime) {
             System.out.println("Error! Invalid punch out time. ");
             return;
         }
-        int hoursWorkedToday = time - punchInTime;
+        double hoursWorkedToday = (time - punchInTime) / 60.0;
         hoursWorked += hoursWorkedToday;
         punchInTime = 0;
+    }
+
+    //    Punch-Out (automatic)
+    public void punchOut() {
+        LocalDateTime now = LocalDateTime.now();
+        int currentTime = now.getHour() * 60 + now.getMinute();
+
+        if (currentTime < punchInTime){
+            System.out.println("Error! Invalid punch out time. ");
+            return;
+        }
+        double hoursWorkedToday = (currentTime - punchInTime) / 60.0;
+        hoursWorked += hoursWorkedToday;
+        punchInTime = 0;
+        System.out.printf("%s Clocked in at %d:%02d%n", name, now.getHour(), now.getMinute());
     }
 
 }
